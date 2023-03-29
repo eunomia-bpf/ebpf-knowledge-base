e7281f6c2280ed7bc0d79e718247f048736d7d66
officeyutong <9004058+Officeyutong@users.noreply.github.com>
2023-03-29 16:35:00 +0800

    fix case issue

--- extract.py
+++ extract.py
@@ -18,7 +18,7 @@ def extract_docs(tutorial_root: pathlib.Path, out_dir: pathlib.Path):
     doc_path = tutorial_root / "src"
 
     copy_file(out_dir, tutorial_root/"README.md", "main")
-    copy_file(out_dir, doc_path/"summary.md", "summary")
+    copy_file(out_dir, doc_path/"SUMMARY.md", "summary")
     for dir in (t for t in os.listdir(doc_path) if os.path.isdir(doc_path/t) and not t.startswith("bcc")):
         copy_file(out_dir, doc_path/dir/"README.md", dir)
     for doc in os.listdir(doc_path/"bcc-documents"):
